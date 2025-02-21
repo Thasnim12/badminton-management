a057137c72a2b858cd06defb23445d67b7354981
Thasni <thasnimaskeva.io@gmail.com>
2025-02-21 19:14:44 +0530

    controller changes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -29,27 +29,28 @@ const {
   editStatus,
   deleteAddons,
   editAddons,
-  getAllslots
+  getAllslots,
 
 } = require("../controllers/adminController");
 
-const { getBookings } = require('../controllers/bookingController')
+const { getBookings,
+  createOfflineBooking,
+  downloadBookings
+} = require('../controllers/bookingController')
 const { downloadDonations } = require('../controllers/donationController')
-const { downloadBookings } = require('../controllers/bookingController')
 
-const { bannerUpload, courtUpload,staffUpload } = require('../helper/multer');
-const authenticateUser = require('../middlewares/userMiddleware');
+const { bannerUpload, courtUpload, staffUpload } = require('../helper/multer');
 
 
 
 const adminroute = express.Router();
 
 //Get route
-adminroute.get('/users',authenticateAdmin,getAllusers)
-adminroute.get('/courts',getAllcourts)
-adminroute.get('/slots/:courtId',authenticateAdmin,getAllSlots)
-adminroute.get('/addons',authenticateAdmin,getAlladdons)
-adminroute.get("/admin-profile", authenticateAdmin,getAdminDetails);
+adminroute.get('/users', authenticateAdmin, getAllusers)
+adminroute.get('/courts', authenticateAdmin, getAllcourts)
+adminroute.get('/slots/:courtId', authenticateAdmin, getAllSlots)
+adminroute.get('/addons', authenticateAdmin, getAlladdons)
+adminroute.get("/admin-profile", authenticateAdmin, getAdminDetails);
 adminroute.get("/users", authenticateAdmin, getAllusers);
 adminroute.get("/courts", getAllcourts);
 adminroute.get("/slots/:courtId", authenticateAdmin, getAllSlots);
@@ -61,7 +62,7 @@ adminroute.get("/get-message", authenticateAdmin, getMessages);
 adminroute.get('/bookings', authenticateAdmin, getBookings)
 adminroute.get('/download-donation', authenticateAdmin, downloadDonations)
 adminroute.get('/download-booking', authenticateAdmin, downloadBookings)
-adminroute.get('/get-slots',getAllslots)
+adminroute.get('/get-slots', authenticateAdmin, getAllslots)
 
 
 //Post route
@@ -70,22 +71,23 @@ adminroute.post("/logout", authenticateAdmin, adminLogout);
 adminroute.post("/courts", authenticateAdmin, addCourt);
 adminroute.post("/slots", authenticateAdmin, generateSlots);
 adminroute.post("/staffs", authenticateAdmin, addStaff);
-adminroute.post('/addons',authenticateAdmin,manageAddons)
-adminroute.post('/banner',authenticateAdmin,addBanner)
+adminroute.post('/addons', authenticateAdmin, manageAddons)
+adminroute.post('/banner', authenticateAdmin, addBanner)
 adminroute.post('/add-user', authenticateAdmin, addUser)
+adminroute.post('/offline-bookings', authenticateAdmin, createOfflineBooking)
 
 //Edit route
 adminroute.put("/users/:userId", authenticateAdmin, manageUsers);
 adminroute.put("/edit-slots/:slotId", authenticateAdmin, updateSlot);
 adminroute.put("/staffs/:employee_id", authenticateAdmin, manageStaffs);
 adminroute.put('/edit-addons/:addonsId', authenticateAdmin, editAddons)
-adminroute.put('/banner/:bannerId',authenticateAdmin,bannerUpload,editBanner)
-adminroute.put('/status',authenticateAdmin,editStatus)
+adminroute.put('/banner/:bannerId', authenticateAdmin, bannerUpload, editBanner)
+adminroute.put('/status', authenticateAdmin, editStatus)
 
 // Delete route
 adminroute.delete("/staffs/:id", authenticateAdmin, deleteStaff);
-adminroute.delete('/delete-court',authenticateAdmin,deleteCourt)
-adminroute.delete('/delete-banner/:bannerId',authenticateAdmin,deleteBanner)
-adminroute.delete('/delete-addons/:addonsId',authenticateAdmin,deleteAddons)
+adminroute.delete('/delete-court', authenticateAdmin, deleteCourt)
+adminroute.delete('/delete-banner/:bannerId', authenticateAdmin, deleteBanner)
+adminroute.delete('/delete-addons/:addonsId', authenticateAdmin, deleteAddons)
 
 module.exports = adminroute;
